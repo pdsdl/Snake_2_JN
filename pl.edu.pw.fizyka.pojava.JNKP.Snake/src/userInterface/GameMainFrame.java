d33d06b8376942fc18ba900ef79f505d781f8731
pdsdl <46254108+pdsdl@users.noreply.github.com>
2020-04-07 19:39:15 +0200

    test

--- pl.edu.pw.fizyka.pojava.JNKP.Snake/src/userInterface/GameMainFrame.java
+++ pl.edu.pw.fizyka.pojava.JNKP.Snake/src/userInterface/GameMainFrame.java
@@ -12,23 +12,25 @@ public class GameMainFrame extends JFrame
 	{
 		initializeLayout();
 	}
-	
+
 	private void initializeLayout()
 	{
 		add(new GamePanel());
-		
+
 		setTitle(Constants.TITLE);
 		setIconImage(ImageFactory.createImage(Image.SNAKE).getImage());
-		
+
 		pack();//dzieki temu i add(new GamePanel()) upewniamy sie, ze okno
 		//bedzie w takim rozmiarze jak chcemy w GamePanel
-		
+
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		//to bedzie zmienione potem, na razie robimy "sztywne" okienko:
 		setLocationRelativeTo(null);
 		setResizable(false);
 		setVisible(true);
 		//karol
-		
+		//hello
+
+
 	}
 }
